461df20e257b4b1bbde81874569560c44171f693
tecs <62474092+tecspda@users.noreply.github.com>
2021-09-29 16:47:46 +0300

    Create colorExt.swift

---
+++ examples/colorExt.swift
@@ -0,0 +1,15 @@
+//
+//  Example for new color
+//  .bordo, .greenSmDark, .greenMdDark
+//
+//  Created by tecspda on 28.09.2021.
+//
+
+import SwiftUI
+
+
+extension Color {
+    static var bordo: Color { return Color.init(red: 181/255, green: 23/255, blue: 1/255) }
+    static var greenSmDark: Color { return Color.init(red: 0/255, green: 172/255, blue: 23/255) }
+    static var greenMdDark: Color { return Color.init(red: 33/255, green: 152/255, blue: 64/255) }
+}
